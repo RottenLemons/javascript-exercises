c0db3086150aced3170ad0ec742ec5ca686b7a6c
RottenLemons <rottenlemons123@gmail.com>
2022-11-28 16:35:57 +0800

    Solve 04

--- 04_removeFromArray/removeFromArray.js
+++ 04_removeFromArray/removeFromArray.js
@@ -1,5 +1,11 @@
-const removeFromArray = function() {
-
+const removeFromArray = function(arr, ...args) {
+    for (const arg of args) {
+        s = arr.indexOf(arg);
+        if (s != -1) {
+            arr.splice(s, 1);
+        }
+    }
+    return arr;
 };
 
 // Do not edit below this line
