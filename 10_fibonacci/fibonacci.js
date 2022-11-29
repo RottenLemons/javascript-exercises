9cd8226afb79da0111dd2954abdacf8eb333d7d7
RottenLemons <rottenlemons123@gmail.com>
2022-11-29 19:36:29 +0800

    Solve 10

--- 10_fibonacci/fibonacci.js
+++ 10_fibonacci/fibonacci.js
@@ -1,5 +1,19 @@
-const fibonacci = function() {
-
+const fibonacci = function(a) {
+    let num = +a;
+    if (num <= 0) {
+        return 'OOPS';
+    } else if (num === 0) {
+        return 0;
+    }
+    let b = 0;
+    let c = 1;
+    let temp = 0;
+    for (let i = 1; i < num; i++) {
+        temp = c;
+        c += b;
+        b = temp;
+    }
+    return c;
 };
 
 // Do not edit below this line
