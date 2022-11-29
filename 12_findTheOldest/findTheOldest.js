c9cf8c52fa2462ca401f845e0558ff2ea1a14896
RottenLemons <rottenlemons123@gmail.com>
2022-11-29 19:58:03 +0800

    Solve 12

--- 12_findTheOldest/findTheOldest.js
+++ 12_findTheOldest/findTheOldest.js
@@ -1,5 +1,13 @@
-const findTheOldest = function() {
-
+const findTheOldest = function(arr) {
+    return arr.sort((obj1, obj2) => {
+        change1 = (obj1.yearOfDeath == undefined) ? (new Date()).getFullYear() - obj1.yearOfBirth: obj1.yearOfDeath - obj1.yearOfBirth;
+        change2 = (obj2.yearOfDeath == undefined) ? (new Date()).getFullYear() - obj2.yearOfBirth: obj2.yearOfDeath - obj2.yearOfBirth;
+        if (change1 > change2) {
+            return -1;
+        } else {
+            return 1;
+        }
+    })[0];
 };
 
 // Do not edit below this line
