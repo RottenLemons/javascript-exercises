eab51e6907ea3b1eee76812b9067510d764fcabb
RottenLemons <rottenlemons123@gmail.com>
2022-11-29 19:27:04 +0800

    Solve 09

--- 09_palindromes/palindromes.js
+++ 09_palindromes/palindromes.js
@@ -1,5 +1,6 @@
-const palindromes = function () {
-
+const palindromes = function (str) {
+    let newstr = str.replace(/[^\w]|_/g, "").toLowerCase();
+    return (newstr == newstr.split('').reverse().join(''));
 };
 
 // Do not edit below this line
